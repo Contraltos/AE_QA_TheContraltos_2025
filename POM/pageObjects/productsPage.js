7a04e626cf1f8f9499aef8a53a541a25fba9a517
Slava <svyatoslavmochalov@gmail.com>
2025-06-09 22:48:03 +0300

    search the product

--- POM/pageObjects/productsPage.js
+++ POM/pageObjects/productsPage.js
@@ -4,12 +4,29 @@ class ProductsPage {
     }
 
     locators = {
-    getCategories: () => this.page.locator("[data-parent='#accordian']")
+    getCategories: () => this.page.locator("[data-parent='#accordian']"),
+    getSearchInput: () => this.page.locator('#search_product'),
+    getSearchButton: () => this.page.locator('#submit_search'),
+    getSearchedProductsTitle: () => this.page.locator('h2.title.text-center'),
+    getAnyProduct: () => this.page.locator('.productinfo.text-center')
 };
 
     async getCategoriesText() {
         return await this.locators.getCategories().allInnerTexts();
     }
+
+    async searchProduct(productName) {
+        await this.locators.getSearchInput().fill(productName);
+        await this.locators.getSearchButton().click();
+    }
+
+    async shouldSearchedProductsVisible() {
+        return await this.locators.getSearchedProductsTitle().textContent();
+    }
+
+    async getSearchResultsCount() {
+        return await this.locators.getAnyProduct().count();
+    }
 }
 
-export default ProductsPage; 
+export default ProductsPage;
