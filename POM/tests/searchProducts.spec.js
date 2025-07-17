b8a3666be2c227bdc2ae288e3ea4622842085dc0
[MariiaHolinko] <[holinkomariia@gmail.com]>
2025-07-17 18:21:45 +0200

    TC_009_01, TC_020_01

--- POM/tests/searchProducts.spec.js
+++ POM/tests/searchProducts.spec.js
@@ -1,9 +1,7 @@
 import { test } from "../helpers/fixtures";
 import { expect } from '@playwright/test';
 
-test.describe('search products test', () => {
-
-    test('verify search field on the product page', async ({ header, productsPage }) => {
+    test('TC_009_01, TC_020_01 - Verify search field on the product page', async ({ header, productsPage, productDetailsPage }) => {
         await header.clickProductsLink(); 
         await productsPage.searchProduct('dress');
 
@@ -12,5 +10,20 @@ test.describe('search products test', () => {
 
         const count = await productsPage.getSearchResultsCount();
         expect(count).toBeGreaterThan(0);
+
+        for (let i = 0; i < count; i++) {
+            await productsPage.openProductByIndex(i);
+            const name = await productDetailsPage.getTitle();
+            const category = await productDetailsPage.getCategory();
+
+            expect(name.includes('Dress') || category.includes('Dress')).toBeTruthy();
+
+            await page.goBack();
+        }
+
     });
-});
+
+    
+
+    
+
