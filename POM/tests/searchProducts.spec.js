6bc9266918ad53f562498fd021d25f15aed8f884
Mariia Holinko <holinkomariia@gmail.com>
2025-07-21 21:01:37 +0200

    TC_009_01, TC_020_01 (#41)
    * TC_009_01, TC_020_01
    
    * searchProduct
    
    * change locators
    
    ---------
    
    Co-authored-by: [MariiaHolinko] <[holinkomariia@gmail.com]>

--- POM/tests/searchProducts.spec.js
+++ POM/tests/searchProducts.spec.js
@@ -1,9 +1,7 @@
 import { test } from "../helpers/fixtures";
 import { expect } from '@playwright/test';
 
-test.describe('search products test', () => {
-
-    test('verify search field on the product page', async ({ header, productsPage }) => {
+    test('TC_009_01, TC_020_01 - Verify search field on the product page', async ({ header, productsPage, productDetailsPage, page}) => {
         await header.clickProductsLink(); 
         await productsPage.searchProduct('dress');
 
@@ -12,5 +10,17 @@ test.describe('search products test', () => {
 
         const count = await productsPage.getSearchResultsCount();
         expect(count).toBeGreaterThan(0);
+
+        for (let i = 0; i < count; i++) {
+            await productsPage.openProductByIndex(i);
+            const category = await productDetailsPage.locators.getProductInfoBlock().textContent();
+            expect(category.toLowerCase()).toContain('dress');
+            await page.goBack();
+        }
+
     });
-});
+
+    
+
+    
+
