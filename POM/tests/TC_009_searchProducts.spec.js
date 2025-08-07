e1a8ee1f43eb23ad375497ab822dbdea0f4f1593
Slava <svyatoslavmochalov@gmail.com>
2025-08-07 14:59:28 +0300

    Rename

---
+++ POM/tests/TC_009_searchProducts.spec.js
@@ -0,0 +1,16 @@
+import { test } from "../helpers/fixtures";
+import { expect } from '@playwright/test';
+
+test.describe('search products test', () => {
+
+    test('verify search field on the product page', async ({ header, productsPage }) => {
+        await header.clickProductsLink(); 
+        await productsPage.searchProduct('dress');
+
+        const searchedTitle = await productsPage.shouldSearchedProductsVisible();
+        expect(searchedTitle).toMatch(/Searched Products/i);
+
+        const count = await productsPage.getSearchResultsCount();
+        expect(count).toBeGreaterThan(0);
+    });
+});
